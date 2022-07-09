b2737b0a9d0fc17c71fbc58d99c499d4581e855b
Butcher Sarai <afarukhshin@gmail.com>
2022-07-09 10:16:25 +0300

    add t_command

--- inc/parser.h
+++ inc/parser.h
@@ -2,5 +2,15 @@
 #ifndef PARSER_H
 # define PARSER_H
 
+# include "lists.h"
+
+typedef struct s_command {
+	char				pipe;
+	int					nb;
+	t_list				*f_in;
+	t_list				*f_out;
+	t_list				*f_ap;
+	t_list				*delim;
+}	t_command;
 
 #endif
