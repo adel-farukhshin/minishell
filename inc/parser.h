57827c94977cb03b062b91aae0dec3523d02f9ba
Butcher Sarai <afarukhshin@gmail.com>
2022-07-30 18:37:35 +0300

    add parse

--- inc/parser.h
+++ inc/parser.h
@@ -4,6 +4,15 @@
 #include "scanner.h"
 #include "node.h"
 
-node *parse(t_token *t);
+/**
+ * @brief 
+ * Takes the list of tokens. Analyzes them in accordance with grammar rules. 
+ * Returns the cmd_node, pipe_node or error_node.
+ * 
+ * @param t 
+ * @return t_node* 
+ */
+t_node *parse(t_token *t);
+
 
 #endif
