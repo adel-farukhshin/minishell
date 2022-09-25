3c41865ff60d73f26e994fb2b24845b0f4b44602
Maria-May <masha.maria050@gmail.com>
2022-09-25 18:23:03 +0300

    add norminette executor files

--- src/executor.c
+++ src/executor.c
@@ -1,158 +1,128 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   executor.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mdaryn <mdaryn@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/25 17:43:18 by mdaryn            #+#    #+#             */
+/*   Updated: 2022/09/25 18:14:37 by mdaryn           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "executor.h"
 #include "clean.h"
 
-char	*find_path(char **envp)
+void	exec_one_cmd(t_node *cmd, t_context	*ctx, t_shell	*shell)
 {
-	while (ft_strncmp("PATH", *envp, 4))
-		envp++;
-	return (*envp + 5);
-}
+	int			children;
+	int			builtin_num;
+	int			status;
+	int			i;
 
-char	*get_cmd(char **paths, char *cmd)
-{
-	char	*tmp;
-	char	*command;
-
-	while (*paths)
-	{
-		tmp = ft_strjoin(*paths, "/");
-		command = ft_strjoin(tmp, cmd);
-		free(tmp);
-        if (access(command, 0) == 0)
-			return (command);            
-        free (command);
-		paths++;
-	}
-	return (NULL);
-}
-
-int    executor(t_node *cmd, t_shell    *shell)
-{
-    t_context   ctx;
-    int children;
-    int i;
-    int builtin_num;
-    int status;
-
-    i = 0;
-    ctx.fd[0] = STDIN_FILENO;
-    ctx.fd[1] = STDOUT_FILENO;
-    ctx.fd_close = -1;
-    checking_heredoc(cmd);
-    if (cmd->type == cmd_node)
-    {
-        builtin_num = check_builtins(cmd);
-        if (builtin_num != BUILTIN_NONE)
-            exe_one_builtin(cmd, &ctx, shell, builtin_num);
-        else
-        {
-            children = execute_cmd(cmd, &ctx, shell);
-            while (i < children)
-            {
-                wait(&status);
-                shell->exit_status = WEXITSTATUS(status);
-                i++;
-            }
-        }
-    }
-    else
-    {
-        children = exec_node(cmd, &ctx, shell);
-        while (i < children)
-        {
-            wait(&status);
-            shell->exit_status = WEXITSTATUS(status);
-            i++;
-        }
-    }
-    return (children);
-}
-
-int    exec_node(t_node *cmd, t_context   *ctx, t_shell    *shell)
-{
-    if (cmd->type == error_node)
-    {
-        printf("error: %s\n", cmd->value.error_msg);
-        return (0);
-    }
-	else if (cmd->type == cmd_node)
-        return (execute_cmd(cmd, ctx, shell));
+	i = 0;
+	builtin_num = check_builtins(cmd);
+	if (builtin_num != BUILTIN_NONE)
+		exe_one_builtin(cmd, ctx, shell, builtin_num);
 	else
-        return (execute_pipe(cmd, ctx, shell));
-        
+	{
+		children = execute_cmd(cmd, ctx, shell);
+		while (i < children)
+		{
+			wait(&status);
+			shell->exit_status = WEXITSTATUS(status);
+			i++;
+		}
+	}
 }
 
-int    execute_cmd(t_node    *cmd, t_context *ctx, t_shell    *shell)
+int	executor(t_node *cmd, t_shell	*shell)
 {
-    pid_t pid;
-    char    *path_str;
-    char    *exe_cmd;
-    char    **cmd_paths;
-    char    **cmd_args;
-    int     builtin_num;
+	t_context	ctx;
+	int			children;
+	int			i;
+	int			status;
 
-    // signal(SIGINT, SIG_IGN);
-	// signal(SIGQUIT, SIG_IGN);
-    pid = fork();
-    if (pid == 0)
-    {
-        make_redir(cmd, ctx);
-        dup2(ctx->fd[STDIN_FILENO], STDIN_FILENO);
-        dup2(ctx->fd[STDOUT_FILENO], STDOUT_FILENO);
-        if (ctx->fd_close >= 0) 
-            close(ctx->fd_close);
-        builtin_num = check_builtins(cmd);
-        if (builtin_num == BUILTIN_NONE)
-        {
-            path_str = find_path(shell->env_arr);
-            cmd_paths = ft_split(path_str, ':');
-            exe_cmd = get_cmd(cmd_paths, (char *)cmd->value.cmd_val.args->val); // почистить
-            if (exe_cmd == NULL)
-                exe_cmd = (char *)cmd->value.cmd_val.args->val;
-            cmd_args = lst_to_char(&cmd->value.cmd_val.args); // почистить
-            shell->exit_status = execve(exe_cmd, cmd_args, shell->env_arr);
-            if (errno != 0)
-            {
-                // ft_putstr_fd()
-                // perror("exe_cmd"); // ft_putstr_fd();
-                if (shell->exit_status == -1)
-                    shell->exit_status = 127;
-                exit(shell->exit_status);
-            }
-            clean_array(cmd_args);
-            clean_array(cmd_paths);
-            free(path_str);
-            free(exe_cmd);
-            exit(shell->exit_status);
-        }
-        else
-            start_buitins_child(cmd, shell, builtin_num);
-    }
-    return (1); // One child was spawned
+	i = 0;
+	ctx.fd[0] = STDIN_FILENO;
+	ctx.fd[1] = STDOUT_FILENO;
+	ctx.fd_close = -1;
+	checking_heredoc(cmd);
+	if (cmd->type == cmd_node)
+	{
+		exec_one_cmd(cmd, &ctx, shell);
+	}
+	else
+	{
+		children = exec_node(cmd, &ctx, shell);
+		while (i < children)
+		{
+			wait(&status);
+			shell->exit_status = WEXITSTATUS(status);
+			i++;
+		}
+	}
+	return (children);
 }
 
-int    execute_pipe(t_node    *cmd, t_context *ctx, t_shell    *shell)
+int	exec_node(t_node	*cmd, t_context	*ctx, t_shell	*shell)
 {
-    t_node  *left;
-    t_node  *right;
-    int ft_pipe[2];
-    t_context   left_ctx;
-    t_context   right_ctx;
-    int children;
-    
-    pipe(ft_pipe);
-    children = 0;
-    left = cmd->value.pipe_val.left;
-    left_ctx = *ctx;
-    left_ctx.fd[STDOUT_FILENO] = ft_pipe[STDOUT_FILENO];
-    left_ctx.fd_close = ft_pipe[STDIN_FILENO];
-    children += exec_node(left, &left_ctx, shell);
-    close(ft_pipe[STDOUT_FILENO]);
-    right = cmd->value.pipe_val.right;
-    right_ctx = *ctx;
-    right_ctx.fd[STDIN_FILENO] = ft_pipe[STDIN_FILENO];
-    right_ctx.fd_close = ft_pipe[STDOUT_FILENO];
-    children += exec_node(right, &right_ctx, shell);
-    close(ft_pipe[STDIN_FILENO]);
-    return (children);
+	int	children;
+
+	children = 0;
+	if (cmd->type == error_node)
+	{
+		printf("error: %s\n", cmd->value.error_msg);
+		return (0);
+	}
+	else if (cmd->type == cmd_node)
+		return (execute_cmd(cmd, ctx, shell));
+	else
+		return (execute_pipe(cmd, ctx, shell, &children));
+}
+
+int	execute_cmd(t_node	*cmd, t_context	*ctx, t_shell	*shell)
+{
+	pid_t	pid;
+	int		builtin_num;
+
+	pid = fork();
+	if (pid == 0)
+	{
+		make_redir(cmd, ctx);
+		dup2(ctx->fd[STDIN_FILENO], STDIN_FILENO);
+		dup2(ctx->fd[STDOUT_FILENO], STDOUT_FILENO);
+		if (ctx->fd_close >= 0)
+			close(ctx->fd_close);
+		builtin_num = check_builtins(cmd);
+		if (builtin_num == BUILTIN_NONE)
+			exec_simple_cmd(cmd, shell);
+		else
+			start_buitins_child(cmd, shell, builtin_num);
+	}
+	return (1);
+}
+
+int	execute_pipe(t_node	*cmd, t_context	*ctx, t_shell	*shell, int *children)
+{
+	t_node		*left;
+	t_node		*right;
+	int			ft_pipe[2];
+	t_context	left_ctx;
+	t_context	right_ctx;
+
+	pipe(ft_pipe);
+	left = cmd->value.pipe_val.left;
+	left_ctx = *ctx;
+	left_ctx.fd[STDOUT_FILENO] = ft_pipe[STDOUT_FILENO];
+	left_ctx.fd_close = ft_pipe[STDIN_FILENO];
+	*children += exec_node(left, &left_ctx, shell);
+	close(ft_pipe[STDOUT_FILENO]);
+	right = cmd->value.pipe_val.right;
+	right_ctx = *ctx;
+	right_ctx.fd[STDIN_FILENO] = ft_pipe[STDIN_FILENO];
+	right_ctx.fd_close = ft_pipe[STDOUT_FILENO];
+	*children += exec_node(right, &right_ctx, shell);
+	close(ft_pipe[STDIN_FILENO]);
+	return (*children);
 }
---
+++ src/executor_2.c
@@ -0,0 +1,56 @@
+#include "executor.h"
+#include "clean.h"
+
+char	*find_path(char **envp)
+{
+	while (ft_strncmp("PATH", *envp, 4))
+		envp++;
+	return (*envp + 5);
+}
+
+char	*get_cmd(char **paths, char *cmd)
+{
+	char	*tmp;
+	char	*command;
+
+	while (*paths)
+	{
+		tmp = ft_strjoin(*paths, "/");
+		command = ft_strjoin(tmp, cmd);
+		free(tmp);
+		if (access(command, 0) == 0)
+			return (command);
+		free(command);
+		paths++;
+	}
+	return (NULL);
+}
+
+void	exec_simple_cmd(t_node	*cmd, t_shell	*shell)
+{
+	char	*path_str;
+	char	*exe_cmd;
+	char	**cmd_paths;
+	char	**cmd_args;
+
+	path_str = find_path(shell->env_arr);
+	cmd_paths = ft_split(path_str, ':');
+	exe_cmd = get_cmd(cmd_paths, (char *)cmd->value.cmd_val.args->val);
+	if (exe_cmd == NULL)
+		exe_cmd = (char *)cmd->value.cmd_val.args->val;
+	cmd_args = lst_to_char(&cmd->value.cmd_val.args);
+	shell->exit_status = execve(exe_cmd, cmd_args, shell->env_arr);
+	if (errno != 0)
+	{
+		// ft_putstr_fd()
+		// perror("exe_cmd"); // ft_putstr_fd();
+		if (shell->exit_status == -1)
+			shell->exit_status = 127;
+		exit(shell->exit_status);
+	}
+	clean_array(cmd_args);
+	clean_array(cmd_paths);
+	free(path_str);
+	free(exe_cmd);
+	exit(shell->exit_status);
+}
