ea4ab273078643eb48cf87a9eb793a1cc3823ebe
Butcher Sarai <bsarai@at-l2.msk.21-school.ru>
2022-09-26 15:38:52 +0300

    norm extend_arg

--- src/extend_arg.c
+++ src/extend_arg.c
@@ -19,101 +19,120 @@
  * @return char 
  */
 
-char	find_ext(char *s, long *len);
+char		find_ext(char *s, long *len);
 
-// @brief
-// Takes s and checks, whether there is an environment variable. If there is a variable, returns 
-// its value. Otherwise, returns null.
-// @return 
 static char	*env_var(char *s);
-// static char *get_env_var(char *s);
-// static void	update_counters(char *s, long *cur, long *start);
 
+char 		loop_exts(char *s, char *start,
+				t_list **list, t_shell *sh);
 
+char		process_ext(char *s, char *start,
+				t_list **list, t_shell *sh);
 
 char extend_arg(t_blist *tok, t_shell *sh)
 {
 	char	*s;
 	char	*start;
-	long	len;
-	char	c;
 	t_list	*list;
 
 	s = (char *)tok->val;
 	start = s;
-	len = 0;
 	list = NULL;
-	
-	// check extensions
 	s = ft_strchr(s, '$');
 	if (!s)
 		return (0);
-
-	while (s)
-	{
-		
-		
-		s = ft_strchr(s, '$');
-		if (!s)
-			break ;
-		c = find_ext(s + 1, &len);
-		if (c)
-		{
-			// add substring before extension
-			*s = '\0';
-			start = ft_strdup(start);
-			if (!start)
-			{
-				lst_clear(&list, free);
-				return (-1);
-			}
-			lst_add_back(&list, lst_new((void *)start));
-
-			// add extension value
-			if (c == 1) // pid
-			{
-				// atoi
-				start = ft_itoa(sh->pid);
-			}
-			else if (c == 2) // ecode
-			{
-				// atoi
-				start = ft_itoa(sh->exit_status);
-			}
-			else // arg
-			{
-				// copy vname
-				start = malloc(len);
-				if (!start)
-				{
-					lst_clear(&list, free);
-					return (-1);
-				}
-				ft_strncpy(start, s + 1, len - 1);
-				// get value
-				start = env_var(start);
-				// printf("var name: %s\n", start);
-			}
-			if (!start)
-			{
-				lst_clear(&list, free);
-				return (-1);
-			}
-			lst_add_back(&list, lst_new((void *)start));
-		}
-		s += len;
-
-		len = 0;
-		start = s + len;
-	}
-	// join
+	if (loop_exts(s, start, &list, sh) == -1)
+		return (-1);
 	tok->val = str_join(list);
 	lst_clear(&list, free);
 	if (!tok->val)
 		return (-1);
-
 	return (0);
 }
+
+char loop_exts(char *s, char *start,
+			t_list **list, t_shell *sh)
+{
+	long	len;
+
+	len = 0;
+	while (s)
+	{
+		s = ft_strchr(s, '$');
+		if (!s)
+			break ;
+		len = process_ext(s, start, list, sh);
+		if (len == -1)
+		{
+			lst_clear(list, free);
+			return (-1);
+		}
+		s += len;
+		start = s;
+	}
+	return (0);
+}
+
+char	add_substring(char *s, char *start, t_list **list);
+t_list	*create_ext(char c, char *s, long len,
+			t_shell *sh);
+
+char	process_ext(char *s, char *start,
+			t_list **list, t_shell *sh)
+{
+	char	c;
+	long	len;
+	t_list	*l;
+
+	len = 0;
+	c = find_ext(s + 1, &len);
+	if (c)
+	{
+		if (add_substring(s, start, list) == -1)
+			return (-1); 
+		l = create_ext(c, s, len, sh);
+		if (!l)
+			return (-1);
+		lst_add_back(list, l);
+	}
+	return (len);
+}
+
+char add_substring(char *s, char *start, t_list **list)
+{
+	*s = '\0';
+	start = ft_strdup(start);
+	if (!start)
+	{
+		lst_clear(list, free);
+		return (-1);
+	}
+	lst_add_back(list, lst_new((void *)start));
+	return (0);
+}
+
+t_list	*create_ext(char c, char *s, long len,
+			t_shell *sh)
+{
+	char	*start;
+
+	if (c == 1)
+		start = ft_itoa(sh->pid);
+	else if (c == 2)
+		start = ft_itoa(sh->exit_status);
+	else
+	{
+		start = malloc(len);
+		if (!start)
+			return (NULL);
+		ft_strncpy(start, s + 1, len - 1);
+		start = env_var(start);
+	}
+	if (!start)
+		return (NULL);
+	return (lst_new((void *)start));
+}
+
 
 char	*str_join(t_list *list)
 {
@@ -133,8 +152,6 @@ char	*str_join(t_list *list)
 	}
 	val = malloc(len + 1);
 	cur = val;
-	if (!val)
-		return (NULL);
 	while (tmp)
 	{
 		len = ft_strlen((char *)tmp->val);
@@ -146,54 +163,6 @@ char	*str_join(t_list *list)
 	return (val);
 }
 
-/*
-char extend_arg(t_blist *tok)
-{
-	char	*s;
-	long	cur;
-	long	start;
-	t_list	*words;
-	char	*var;
-	cur = 0;
-	start = 0;
-	s = tok->val;
-	words = NULL;
-	while (s[cur++])
-	{
-		if (s[cur] == '$') // replace with strchr
-		{
-			if (s[cur + 1] == '$' || s[cur + 1] == '?' || 
-				(s[cur + 1] >= 'a' && s[cur + 1] <= 'z') ||
-				(s[cur + 1] >= 'A' && s[cur + 1] <= 'Z') ||
-				s[cur + 1] == '_')
-			{
-				s[cur] = '\0';
-				lst_add_back(&words, lst_new((void *)ft_strdup(s + start)));
-				if (s[cur + 1] == '$') // PID
-					// ft_atoi
-					lst_add_back(&words, lst_new((void *)ft_strdup("4242")));
-				else if (s[cur + 1] == '?') // ECODE
-					// ft_atoi
-					lst_add_back(&words, lst_new((void *)ft_strdup("777")));
-				else
-				{
-					var = env_var(s + cur + 1);
-					if (var)
-						lst_add_back(&words, lst_new(var));
-				}
-				update_counters(s + cur + 1, &cur, &start);
-				// update start and cur
-			}
-				
-		}
-	}
-	printf("extension: %s\n", (char *)words->val);
-	// be careful in joining: strdup can return null
-	// null the tok->val
-	return (0);
-}
-*/
-
 char	find_ext(char *s,  long *len)
 {
 	long	i;
@@ -203,7 +172,7 @@ char	find_ext(char *s,  long *len)
 	if (s[i] == '$' || s[i] == '?')
 	{
 		*len += 1;
-		if (s[i] == '$') // PID
+		if (s[i] == '$')
 			return (1);
 		return (2);
 	}
@@ -223,27 +192,6 @@ char	find_ext(char *s,  long *len)
 	return (0);
 }
 
-
-	// *len = i;
-	// while (s && s[i++])
-	// {
-	// 	s = ft_strchr(s, '$');
-	// 	if (s[i] == '$') // replace with strchr
-	// 	{
-	// 		if (s[i + 1] == '$' || s[i + 1] == '?' || 
-	// 			(s[i + 1] >= 'a' && s[i + 1] <= 'z') ||
-	// 			(s[i + 1] >= 'A' && s[i + 1] <= 'Z') ||
-	// 			s[i + 1] == '_')
-	// 		{
-	// 			*st += i;
-	// 			if (s[i + 1] == '$' || s[i + 1] == '?')
-	// 				*end += i + 1; 
-	// 		}
-	// 	}
-	// }
-// }
-
-
 static char	*env_var(char *s)
 {
 	char	*val;
@@ -258,55 +206,3 @@ static char	*env_var(char *s)
 	free(s);
 	return (ret);
 }
-
-// static char *get_env_var(char *s)
-// {
-// 	long	i;
-// 	char	c;
-// 	char	*val;
-
-// 	i = 0;
-// 	while (s[i++])
-// 	{
-// 		c = s[i];
-// 		if ((c >= 'a' && c <= 'z') || 
-// 			(c >= 'A' && c <= 'Z') ||
-// 			(c == '_'))
-// 			continue ;
-// 		else if (i && c >= '0' && c <= '9')
-// 			continue ;		
-// 		else
-// 			break ;
-// 	}
-// 	// printf("%d\n", i);
-// 	val = malloc(i + 1);
-// 	if (!val)
-// 		return (val);
-// 	ft_strncpy(val, s, i);
-// 	return (val);
-// }
-
-// static void	update_counters(char *s, long *cur, long *start)
-// {
-// 	long	i;
-// 	char	c;
-
-// 	i = 0;
-// 	if (*s == '$' || *s == '?')
-// 		*cur += 1;
-// 	else
-// 		while (s[i++])
-// 		{
-// 			c = s[i];
-// 			if ((c >= 'a' && c <= 'z') || 
-// 				(c >= 'A' && c <= 'Z') ||
-// 				(c == '_'))
-// 				continue ;
-// 			else if (i && c >= '0' && c <= '9')
-// 				continue ;		
-// 			else
-// 				break ;
-// 		}
-// 	*cur += i;
-// 	*start = *cur;
-// }
