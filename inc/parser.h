8726695ac0d2298a1072e3a776dfc9debf03c4c3
Butcher Sarai <bsarai@at-l2.msk.21-school.ru>
2022-09-26 18:13:12 +0300

    norm: parser.h

--- inc/parser.h
+++ inc/parser.h
@@ -1,10 +1,10 @@
 #ifndef PARSER_H
 # define PARSER_H
 
-#include "scanner.h"
-#include "node.h"
-#include "linked_lists.h"
-#include "minishell.h"
+# include "scanner.h"
+# include "node.h"
+# include "linked_lists.h"
+# include "minishell.h"
 
 /**
  * @brief 
@@ -14,15 +14,20 @@
  * @param t 
  * @return t_node* 
  */
-t_node  *parse(t_l_list *token_list, t_shell *sh);
+t_node	*parse(t_l_list *token_list, t_shell *sh);
 
 // parser_parse_arg.c
 char	parse_arg(t_l_list *t, char **ret_val, t_shell *sh);
-char 	add_args(t_l_list *t, t_node *cmd, t_shell *sh);
+char	add_args(t_l_list *t, t_node *cmd, t_shell *sh);
+
+char	process_ext(char *s, char *start,
+			t_list **list, t_shell *sh);
+char	find_ext(char *s, long *len);
 
 /**
  * @brief 
- * Joins all the values of list together. Return the joined string or null. Does not
+ * Joins all the values of list together. 
+ * Return the joined string or null. Does not
  * free list.
  * 
  * @param list 
@@ -36,7 +41,6 @@ char	*str_join(t_list *list);
  * 
  * @return 0 - extension isn't needed; 1 - successful extension; -1 - error
  */
-char extend_arg(t_blist *tok, t_shell *sh);
-
+char	extend_arg(t_blist *tok, t_shell *sh);
 
 #endif
